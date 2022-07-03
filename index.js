ef0e42b5d5570b14ae659224057f5e38cd36f75b
dL-hx <897057246@qq.com>
2022-07-04 07:40:37 +0800

    add:添加校验方法

--- index.js
+++ index.js
@@ -2,3 +2,15 @@
 let hasDecimal = score % 1 !==0
 // 计算是否含有整数
 let integer =  Math.floor(score)
+
+var _toString = Object.prototype.toString; // 原型上的toString方法
+
+// 如何判断是一个纯粹的Object对象
+function isPlainObject(obj) {
+    return _toString.call(obj) === '[object Object]' 
+}
+
+// 如何判断是一个正则对象
+function isRegExp(v) {
+    return _toString.call(v) === '[object RegExp]' 
+}
